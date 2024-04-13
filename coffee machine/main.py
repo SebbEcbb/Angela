40266b06f9294fe03697a694c458af1071c931b8
Sebb <ecbbadel@gmail.com>
2024-04-14 00:40:52 +0200

    Completed coffee machine project

--- coffee machine/main.py
+++ coffee machine/main.py
@@ -33,6 +33,7 @@ resources = {
 }
 
 payments = {
+    "dollars": 0,
     "quarters": 0,
     "dimes": 0,
     "nickles": 0,
@@ -42,6 +43,8 @@ payments = {
 
 
 def make_espresso():
+    print("-----------------\n")
+    print("You chose espresso.")
     # gather information
     water = MENU["espresso"]["ingredients"]["water"]
     coffee = MENU["espresso"]["ingredients"]["coffee"]
@@ -53,19 +56,13 @@ def make_espresso():
 
     pay(cost)
 
-    print("-----------------\n")
-
-    print(f"Your drink is being prepared.\n")
-
-    print(f"Your drink is ready. Thank you!")
-
-    update_resources(water, coffee, milk)
-
-    new_order()
+    process(water, coffee, milk)
 
 
 def make_latte():
-   # gather information
+    print("-----------------\n")
+    print("You chose latte.")
+    # gather information
     water = MENU["latte"]["ingredients"]["water"]
     coffee = MENU["latte"]["ingredients"]["coffee"]
     milk = MENU["latte"]["ingredients"]["milk"]
@@ -76,15 +73,7 @@ def make_latte():
 
     pay(cost)
 
-    print("-----------------\n")
-
-    print(f"Your drink is being prepared.\n")
-
-    print(f"Your drink is ready. Thank you!")
-
-    update_resources(water, coffee, milk)
-
-    new_order()
+    process(water, coffee, milk)
 
 
 def make_cappuccino():
@@ -99,11 +88,15 @@ def make_cappuccino():
 
     pay(cost)
 
+    process(water, coffee, milk)
+
+
+def process(water, coffee, milk):
     print("-----------------\n")
 
-    print(f"Your drink is being prepared.\n")
+    print(f"Drink is being prepared.\n")
 
-    print(f"Your drink is ready. Thank you!")
+    print(f"Drink is ready. Thank you!")
 
     update_resources(water, coffee, milk)
 
@@ -114,8 +107,9 @@ def print_report():
     print("-----------------------")
     print(f"Water: {resources["water"]}")
     print(f"Coffee: {resources["coffee"]}")
-    print(f"Mik: {resources["milk"]}\n")
+    print(f"Milk: {resources["milk"]}\n")
     print("-----------------------")
+    print(f"Dollars: {payments["dollars"]}")
     print(f"Quarters: {payments["quarters"]}")
     print(f"Dimes: {payments["dimes"]}")
     print(f"Nickels: {payments["nickles"]}")
@@ -127,35 +121,59 @@ def print_report():
 
 def new_order():
     # Get customers order
-    choice = input("What would you like? (enter: 'e' or 'espresso'/'l' or 'latte'/ 'c' or 'cappuccino'):\n")
+    e = "'e' for espresso\n"
+    t = "'l' for latte\n"
+    c = "'c' for cappuccino\n"
+    a = "'a' for admin. Requires password.\n"
+
+    choice = input(f"Please make your choice? \n {e} {t} {c} {a}")
 
     if choice == "e" or choice == "espresso":
-        print("You chose espresso.")
         make_espresso()
     elif choice == "l" or choice == "latte":
-        print("You chose latte.")
         make_latte()
     elif choice == "c" or choice == "cappuccino":
         print("You chose cappuccino.")
         make_cappuccino()
-    elif choice == "report":
-        print("You select report.")
-        print_report()
-    elif choice == "off":
-        print("Machine shutting down. Thank you and bye bye!")
-        sys.exit()
+    elif choice == "a":
+        admin()
     else:
         print('Please enter a valid command.')
         new_order()
 
 
+def admin():
+
+    password = "coffee"
+    pw = input("Enter your password\n")
+    if pw == "exit":
+        new_order()
+    elif pw == password:
+        admin_choice = input("'r' for report\n'f' for fill\n'o' for machine off.\n")
+        if admin_choice == "r":
+            print("Print a report.")
+            print_report()
+        elif admin_choice == "f":
+            print("Fill machine.")
+            fill()
+        elif admin_choice == "o":
+            print("Machine is shutting down. Thank you and bye bye!")
+            sys.exit()
+    else:
+        print("Please try again. Enter 'exit' to exit")
+        admin()
+
+
 def check_resources(water, coffee, milk):
     if resources["water"] - water <= 0:
-        end()
+        print("Please fill water first. Chose 'Fill'.")
+        new_order()
     elif resources["coffee"] - coffee <= 0:
-        end()
+        print("Please fill coffee first. Chose 'Fill'.")
+        new_order()
     elif resources["milk"] - milk <= 0:
-        end()
+        print("Please fill milk first. Chose 'Fill'.")
+        new_order()
 
 
 def end():
@@ -163,18 +181,51 @@ def end():
     new_order()
 
 
+def fill():
+    done = False
+
+    while not done:
+
+        refill = input("What do you want to fill ?\n'w' for water\n'c' for coffee\n'm' for milk\n")
+        quantity = int(input("Enter a quantity\n"))
+
+        if refill == "w":
+            resources["water"] += quantity
+        if refill == "c":
+            resources["coffee"] += quantity
+        if refill == "m":
+            resources["milk"] += quantity
+
+        q = input("Are you done refilling?(y/n")
+        if q == "y":
+            done = True
+
+    new_order()
+
+
 def pay(cost):
-    print(f' Costs: {cost}. Please enter you payment')
+
+    print(f' Your drink costs $ {cost}. Please enter coins')
     paid = 0
     while paid < cost:
-        coin = input("Please insert a coin :type  'q' for quarter, 'd' for dimes, 'n' for nickel and 'p' for penny :")
+        d = "'d' for dollars\n"
+        q = "'q' for quarters\n"
+        m = "'m' for dimes\n"
+        n = "'n' for nickel\n"
+        p = "'p' for penny\n"
+        coin = input(f"{d}{q}{m}{n}{p}\n")
+
+        if coin == "d":
+            payments["dollars"] += 1
+            paid += 1
+            print(f"remaining : {cost - paid}.")
 
         if coin == "q":
             payments["quarters"] += 1
             paid += 0.25
             print(f"remaining : {cost - paid}.")
 
-        if coin == "d":
+        if coin == "m":
             payments["dimes"] += 1
             paid += 0.10
             print(f"remaining : {cost - paid}.")
@@ -192,7 +243,7 @@ def pay(cost):
     if paid > cost:
         change = round(paid - cost, 2)
         print(f"You paid $ {paid}")
-        print(f"Your change is {change}.")
+        print(f"Your change is $ {change}.")
 
     payments["total"] += cost
 
